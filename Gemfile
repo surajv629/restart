053436a86eee09a9922df76e52fbe3de692c37f5
Suraj Vishwanath <surajv629@gmail.com>
2015-11-28 00:45:54 +0530

    devise gem install

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
+gem 'devise'
 
 group :doc do
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #heroku requirement
+  #Note to set this to actual host
+  config.action_mailer.default_url_options = { :host => 'restartfresh.herokuapp.com' }
 end
